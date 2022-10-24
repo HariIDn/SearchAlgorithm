b56e2639c625b78287339afdd4214538a1b75adf
Aditya Hari <aditya.hari.ft21@mail.umy.ac.id>
2022-10-24 16:49:16 +0700

    Add BinarySearch

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -39,6 +39,41 @@ namespace SearchAlgorithm
                 arr[i] = Int32.Parse(s1);
             }
         }
+
+        public void BinarySearch()
+        {
+            char ch;
+            do
+            {
+                Console.Write("\nEnter element you want to search: ");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                int lowerbound = 0;
+                int upperbound = n - 1;
+
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + "found at position" + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array\n");
+                Console.WriteLine("\nNumber of Comparison: " + ctr);
+
+                Console.Write("\nContinue Searching? (n/y): ");
+                ch = char.Parse(Console.ReadLine());
+            } while ((ch == 'y') || (ch == 'Y'));
+        }
         static void Main(string[] args)
         {
         }
