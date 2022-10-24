5691735067eabe047027d1c1b131ddfabfac65bb
Aditya Hari <aditya.hari.ft21@mail.umy.ac.id>
2022-10-24 16:26:53 +0700

    add input method

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -8,7 +8,37 @@ namespace SearchAlgorithm
 {
     class Program
     {
+        int[] arr = new int[20];
 
+        int n;
+
+        int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter The Number of Elements in Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements.\n");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Enter array element");
+            Console.WriteLine("-------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (1 + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
         static void Main(string[] args)
         {
         }
